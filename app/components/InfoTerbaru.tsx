3b58aa1717b4132bab9171058993152665c10789
aditrachman <aditrachman23@gmail.com>
2026-09-08 21:32:42 +0700

    style: bedakan warna aksen heading - info teal, proker hijau, rekrutmen biru, kenapa gabung pink

--- app/components/InfoTerbaru.tsx
+++ app/components/InfoTerbaru.tsx
@@ -72,7 +72,7 @@ export default function InfoTerbaru() {
   return (
     <section className="heading-module-scss-module__ZBj6zq__wrapper" data-display="inline" id="info">
       <div className="container">
-        <div className="tagline-module-scss-module__R8CpfG__wrapper color-blue-3">
+        <div className="tagline-module-scss-module__R8CpfG__wrapper color-sea-2">
           <div className="tagline-module-scss-module__R8CpfG__inner">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 18 18">
               <path fill="#353241" d="M10 0H8v18h2z" />
@@ -85,7 +85,7 @@ export default function InfoTerbaru() {
         </div>
         <div className="heading-module-scss-module__ZBj6zq__inner">
           <div className="heading-module-scss-module__ZBj6zq__heading">
-            <h2 className="color-blue-1">Info{" "}<strong>Terbaru<span className="underline"><svg className="underline-squiggle stroke-blue-3" xmlns="http://www.w3.org/2000/svg" width="158" height="18" fill="none" viewBox="0 0 158 18" preserveAspectRatio="none"><path stroke="#353241" strokeWidth="25" d="M1 15c4.603-1.68 15.483-6.046 22.178-10.077C31.548-.115 34.895 15 41.172 15S53.307.389 63.35 3.412c9.743 2.933 8.632 17.127 26.208 6.547C107.133-.621 115.352 25.164 156 3.412" /></svg></span></strong></h2>
+            <h2 className="color-blue-1">Info{" "}<strong>Terbaru<span className="underline"><svg className="underline-squiggle stroke-sea-3" xmlns="http://www.w3.org/2000/svg" width="158" height="18" fill="none" viewBox="0 0 158 18" preserveAspectRatio="none"><path stroke="#353241" strokeWidth="25" d="M1 15c4.603-1.68 15.483-6.046 22.178-10.077C31.548-.115 34.895 15 41.172 15S53.307.389 63.35 3.412c9.743 2.933 8.632 17.127 26.208 6.547C107.133-.621 115.352 25.164 156 3.412" /></svg></span></strong></h2>
           </div>
         </div>
         <div className="info-card">
