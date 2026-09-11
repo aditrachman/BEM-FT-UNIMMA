3e03648812ebff3d3ce3613eeb482933b46659b2
aditrachman <aditrachman23@gmail.com>
2026-09-11 09:25:34 +0700

    feat: tambah logo DPM + himpunan (Himanifo/HMTI/HMTM/HMTO) di section keluarga

--- app/components/CustomerLogos.tsx
+++ app/components/CustomerLogos.tsx
@@ -3,6 +3,11 @@ export default function CustomerLogos() {
     { src: "/LogoUNIMMA.png", alt: "Logo UNIMMA" },
     { src: "/Logofakultas.png", alt: "Logo Fakultas Teknik UNIMMA", plain: true },
     { src: "/logo.png", alt: "Logo BEM FT UNIMMA" },
+    { src: "/images/logos/dpm.png", alt: "Logo DPM" },
+    { src: "/images/logos/himanifo.png", alt: "Logo Himanifo" },
+    { src: "/images/logos/hmti.png", alt: "Logo HMTI" },
+    { src: "/images/logos/hmtm.png", alt: "Logo HMTM" },
+    { src: "/images/logos/hmto.png", alt: "Logo HMTO" },
   ];
   return (
     <section className="keluarga">
